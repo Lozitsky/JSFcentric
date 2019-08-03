7ab175e64f3dacdd1084a6f8159af02bac0e815c
Kirilo Lozitsky <bondxxl01@gmail.com>
2019-08-03 06:08:26 +0300

    changed files for jsp 2.2 and above

--- src/main/java/com/kirilo/beans/MainBean.java
+++ src/main/java/com/kirilo/beans/MainBean.java
@@ -4,17 +4,21 @@ import com.kirilo.objects.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.faces.bean.SessionScoped;
+
 //import javax.enterprise.context.SessionScoped;
 //import javax.inject.Named;
 
-import javax.faces.bean.SessionScoped;
-
 
 @Component
+//@Named("registration")
+//@RequestScoped
 @SessionScoped
+//public class MainBean implements Serializable {
 public class MainBean {
 
     @Autowired
+//    @Inject
     private Car car;
 
     public String getSound() {
